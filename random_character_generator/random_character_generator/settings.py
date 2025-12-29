208e213fc8713eb48fe1e35e9a08d9b3d436e3e1
Richard Hamilton <rham153@wgu.edu>
2025-12-29 16:56:09 -0600

    Add core page Add connections to generator

--- random_character_generator/random_character_generator/settings.py
+++ random_character_generator/random_character_generator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3(zgiqip9ji$qiep%fbazc&9sfpt@xui$t$_$_v=nle1l-wzz!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'characters',
+    'core',
 ]
 
 MIDDLEWARE = [
